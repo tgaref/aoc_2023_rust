c25db68a36df3e490241937448d10840acd9ff62
Theo Garefalakis <tgaref@gmail.com>
2023-12-29 09:52:43 +0200

    Day 23 complete

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod day2;
 mod day20;
 mod day21;
 mod day22;
+mod day23;
 mod day3;
 mod day4;
 mod day5;
@@ -39,6 +40,7 @@ use day2::Day2;
 use day20::Day20;
 use day21::Day21;
 use day22::Day22;
+use day23::Day23;
 use day3::Day3;
 use day4::Day4;
 use day5::Day5;
@@ -83,11 +85,12 @@ fn main() {
     let mut day20 = Day20::new();
     let mut day21 = Day21::new();
     let mut day22 = Day22::new();
+    let mut day23 = Day23::new();
 
     let mut days: Vec<&mut dyn Puzzle> = vec![
         &mut day1, &mut day2, &mut day3, &mut day4, &mut day5, &mut day6, &mut day7, &mut day8,
         &mut day9, &mut day10, &mut day11, &mut day12, &mut day13, &mut day14, &mut day15,
-        &mut day16, &mut day17, &mut day18, &mut day19, &mut day20, &mut day21, &mut day22
+        &mut day16, &mut day17, &mut day18, &mut day19, &mut day20, &mut day21, &mut day22, &mut day23
     ];
 
     match selection {
